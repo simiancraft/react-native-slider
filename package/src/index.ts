de967d3b4d85b7dec7c1bc573c247d026f2649ca
the-simian <harlinjesse@gmail.com>
2025-05-09 00:02:04 -0500

    chore: rebuild without DOM

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,11 +1,3 @@
-import { Platform } from 'react-native';
-
-let RNCSlider;
-
-if (Platform.OS === 'web') {
-  RNCSlider = require('./RNCSliderNativeComponent.web').default;
-} else {
-  RNCSlider = require('./RNCSliderNativeComponent.native').default; // Use .native.js file
-}
+const RNCSlider = require('./RNCSliderNativeComponent').default;
 
 export default RNCSlider;
